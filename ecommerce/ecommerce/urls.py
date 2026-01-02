b14172a8ac510f19e84983e5d17b5222e26670f4
Gustavo Salinas <gsalinas1506@uc.cl>
2026-01-02 01:25:41 -0300

    add base models

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 """
 URL configuration for ecommerce project.
@@ -23,4 +25,4 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    password = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=10)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.CharField(max_length=250, default='', blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/product/')
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    address = models.CharField(max_length=100, default='', blank=True, null=True)
+    phone = models.CharField(max_length=20, default='', blank=True, null=True)
+    date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.product)
